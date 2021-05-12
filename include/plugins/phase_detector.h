6222c5088fe2c5512e50a6ed59ab3aa8affbb8fa
sadko4u <sadko4u@gmail.com>
2021-05-12 15:50:20 +0300

    Initial import of the empty project

---
+++ include/plugins/phase_detector.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2021 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-phase-detector
+ * Created on: 12 мая 2021 г.
+ *
+ * lsp-plugins-phase-detector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-phase-detector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-phase-detector. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PLUGINS_PHASE_DETECTOR_H_
+#define PLUGINS_PHASE_DETECTOR_H_
+
+
+
+
+
+#endif /* PLUGINS_PHASE_DETECTOR_H_ */
